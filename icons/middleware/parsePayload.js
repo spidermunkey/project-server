a5038580721d3083d54d8533be24420879e37ffa
spider <burkstremane@gmail.com>
2025-08-06 14:12:06 -0600

    consolidating project apis

--- icons/middleware/parsePayload.js
+++
@@ -1,7 +0,0 @@
-module.exports = function parsePayload(request,response,next){
-  if (request.method == 'POST' && !request.body.payload){
-    response.json({message: 'operation unsuccessful', success: false, reason: 'invalid payload'})
-    return;
-  }
-  next()
-}
--- routes/trackers.js
+++
@@ -1,9 +0,0 @@
-const services = require('../controllers/trackers.js');
-const express = require('express');
-const router = express.Router();
-
-router.get('/meta', services.getTrackers);
-router.post('/api', services.addTracker);
-router.delete('/api', services.deleteTracker);
-
-module.exports = router;
